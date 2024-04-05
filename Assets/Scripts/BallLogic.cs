cf056888377c967949af247d9711bc2d8f8d2408
Aaron <2klaus.4peter@gmail.com>
2024-04-05 23:23:33 +0200

    fix cannonball collision with water barrier

--- Assets/Scripts/BallLogic.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class BallLogic : MonoBehaviour {
-	private void OnCollisionEnter2D(Collision2D collision) {
-		Destroy(gameObject, 0.1f);
-	}
-}
---
+++ Assets/Scripts/BallsLogic.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class BallsLogic : MonoBehaviour {
+	
+	private Rigidbody2D rb;
+	
+	private void OnEnable() {
+		rb = GetComponent<Rigidbody2D>();
+		rb.velocity = Vector2.left * 3;
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision) {
+		Destroy(gameObject, 0.1f);
+	}
+}
