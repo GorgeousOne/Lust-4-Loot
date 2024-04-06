2474da97566e965cb9b6bf555ecab3f09670df78
Aaron <2klaus.4peter@gmail.com>
2024-04-06 18:16:56 +0200

    fix 2 player controls (wrong keys)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,47 +3,37 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour {
-	//input fields
 
-	public InputActionAsset inputAsset;
-	public String actionMapName;
-	
+	public int playerNumber = 1;
 	public GameObject bulletPrefab;
 	public float speed = 5f;
 	public float bulletSpeed = 3f;
 	
-	private InputActionMap controls;
-	private InputAction move;
 	private Vector2 inputVel;
 	private Rigidbody2D rb;
 	
 	private void OnEnable() {
 		rb = GetComponent<Rigidbody2D>();
-		controls = inputAsset.FindActionMap(actionMapName);
-		Debug.Log("found map " + actionMapName + " " + controls);
-		move = controls["Move"];
-		controls["Shoot"].performed += OnShoot;
-		
-		controls.Enable();
 	}
 
-	private void OnDisable() {
-		controls.Disable();
-	}
-
-	private void OnShoot(InputAction.CallbackContext context){
+	public void OnShoot(){
+		int playerFacing = playerNumber == 1 ? 1 : -1;
 		GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
-		bullet.layer = LayerMask.NameToLayer("OurBullet");
-		Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
-		rb.velocity = Vector2.right * bulletSpeed;
+
+		bullet.layer = LayerMask.NameToLayer("Bullet" + playerNumber);
+		
+		Rigidbody2D billetRb = bullet.GetComponent<Rigidbody2D>();
+		billetRb.velocity = Vector2.right * bulletSpeed * playerFacing;
+
+		Debug.Log("TOOT" + playerNumber);
 	}
 
 	// private void OnDisable() {
 	// 	controls.Player.Disable();
 	// }
 
-	private void Update() {
-		inputVel = move.ReadValue<Vector2>();
+	public void OnMove( InputAction.CallbackContext context ) {
+		inputVel = context.ReadValue<Vector2>();
 	}
 
 	private void FixedUpdate() {
