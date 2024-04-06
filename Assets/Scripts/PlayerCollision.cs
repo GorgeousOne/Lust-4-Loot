940355cbaeb042dfb8eafbde66a97038c302a450
Hobbit <hobbit@p-byte.de>
2024-04-06 18:37:44 +0200

    fixed Player Numbers and Layers

--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -4,22 +4,14 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour {
-	
-	public GameObject heartPrefab;
-	public Vector2 heartsPos;
-	public float spacing = 1;
 
-	public int maxHealth = 5;
-	private int currentHealth;
-	private List<GameObject> hearts = new(); // List to store heart GameObjects
+	public int playerNumber = 1;
 
 	public float stackOffset = .5f;
 	public float stackDist = .2f;
 	private List<GameObject> hoardedItems = new();
 	
 	private void OnEnable() {
-		currentHealth = maxHealth;
-		UpdateHearts();
 	}
 
 	private void Update() {
@@ -29,31 +21,13 @@ public class PlayerCollision : MonoBehaviour {
 	}
 
 	private void TakeDamage() {
-		currentHealth -= 1;
-		UpdateHearts();
-
-		if (currentHealth == 0) {
-		}
-	}
-
-	private void UpdateHearts() {
-		// Destroy existing heart GameObjects
-		foreach (GameObject heart in hearts) {
-			Destroy(heart);
-		}
-		hearts.Clear();
-
-		for (int i = 0; i < currentHealth; i++) {
-			GameObject newHeart = Instantiate(heartPrefab, heartsPos + Vector2.right * spacing * i, Quaternion.identity, null);
-			hearts.Add(newHeart);
-		}
 	}
 	
 	private void PickupItem(GameObject item) {
 		Rigidbody2D rb = item.GetComponent<Rigidbody2D>();
 		rb.velocity = Vector2.zero;
 		item.transform.parent = transform;
-		item.layer = LayerMask.NameToLayer("Hoarded");
+		item.layer = LayerMask.NameToLayer("Hoarded" + playerNumber);
 		ItemLogic itemLogic = item.GetComponent<ItemLogic>();
 		itemLogic.onCannonBallHit.AddListener(OnItemHit);
 		
