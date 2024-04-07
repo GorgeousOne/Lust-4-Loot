2510fa7b3fdda52f93cacac8b4526303b02f2a31
Aaron <2klaus.4peter@gmail.com>
2024-04-07 18:30:00 +0200

    clean up title screen

--- Assets/GameStarter.cs
+++ Assets/GameStarter.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class GameStarter : MonoBehaviour {
 	
 	public Canvas menuCanvas;
-
+	public GameObject scoreSlider;
+	public GameObject island;
+	
 	public void OnButtonPress() {
 		//hide the menu on start button press
 		menuCanvas.gameObject.SetActive(false);
+		scoreSlider.SetActive(true);
+		island.SetActive(true);
 		
 		//find all PlayerMovement scripts and enable them
 		PlayerMovement[] players = FindObjectsOfType<PlayerMovement>();
