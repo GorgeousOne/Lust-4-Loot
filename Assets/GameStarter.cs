9950dacdde9d3f34907f64bea94c6ff5d596f338
Aaron <2klaus.4peter@gmail.com>
2024-04-07 18:45:42 +0200

    clean game over screen

--- Assets/GameStarter.cs
+++ Assets/GameStarter.cs
@@ -7,12 +7,19 @@ public class GameStarter : MonoBehaviour {
 	public Canvas menuCanvas;
 	public GameObject scoreSlider;
 	public GameObject island;
+	public GameObject itemSpawner;
+	public GameObject player1;
+	public GameObject player2;
 	
 	public void OnButtonPress() {
 		//hide the menu on start button press
 		menuCanvas.gameObject.SetActive(false);
 		scoreSlider.SetActive(true);
 		island.SetActive(true);
+		itemSpawner.SetActive(true);
+		
+		player1.transform.position = new Vector3(-7, 0, 0);
+		player2.transform.position = new Vector3(7, 0, 0);
 		
 		//find all PlayerMovement scripts and enable them
 		PlayerMovement[] players = FindObjectsOfType<PlayerMovement>();
